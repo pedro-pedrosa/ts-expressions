745564a8404c2217b009c15f28061c7b3076a001
Pedro Pedrosa <blastamos@outlook.com>
2018-06-14 11:12:18 +0100

    added kind checks to expressionKinds test

--- tests/expressionKinds.js
+++ tests/expressionKinds.js
@@ -1,6 +1,7 @@
 const exprtests = require('./lib/tests');
 const ExpressionBuilder = require('../lib/expressions/ExpressionBuilder');
-const BinaryExpression = require('../lib/expressions/BinaryExpression');
+const { ExpressionKind } = require('../lib/expressions/ExpressionKind');
+const { BinaryOperator } = require('../lib/expressions/BinaryExpression');
 
 describe('expression kinds', () => {
     let compiledTest;
@@ -25,45 +26,52 @@ describe('expression kinds', () => {
         expect(compiledTest.out.default.constantNumber).toMatchObject((() => {
             return ExpressionBuilder.constant(5);
         })());
+        expect(compiledTest.out.default.constantNumber.kind).toBe(ExpressionKind.constant);
     });
     test('converts a constant string', () => {
         expect(compiledTest.out.default.constantString).toMatchObject((() => {
             return ExpressionBuilder.constant('str');
         })());
+        expect(compiledTest.out.default.constantString.kind).toBe(ExpressionKind.constant);
     });
     test('converts a constant boolean', () => {
         expect(compiledTest.out.default.constantBoolean).toMatchObject((() => {
             return ExpressionBuilder.constant(true);
         })());
+        expect(compiledTest.out.default.constantBoolean.kind).toBe(ExpressionKind.constant);
     });
     test('converts an empty array', () => {
         expect(compiledTest.out.default.emptyArray).toMatchObject((() => {
             return 'todo';
         })());
+        expect(compiledTest.out.default.emptyArray.kind).toBe(ExpressionKind.arrayLiteral);
     });
     test('converts a number array', () => {
         expect(compiledTest.out.default.numberArray).toMatchObject((() => {
             return 'todo';
         })());
+        expect(compiledTest.out.default.numberArray.kind).toBe(ExpressionKind.arrayLiteral);
     });
     test('converts a variable', () => {
         expect(compiledTest.out.default.variableAccess).toMatchObject((() => {
             return ExpressionBuilder.constant(scopeVariable);
         })());
+        expect(compiledTest.out.default.variableAccess.kind).toBe(ExpressionKind.constant);
     });
     test('converts a binary equals', () => {
         expect(compiledTest.out.default.binaryEquals).toMatchObject((() => {
             return ExpressionBuilder.binary(
                 ExpressionBuilder.constant(scopeVariable),
-                BinaryExpression.BinaryOperator.equals,
+                BinaryOperator.equals,
                 ExpressionBuilder.constant(5));
         })());
+        expect(compiledTest.out.default.binaryEquals.kind).toBe(ExpressionKind.binary);
     });
     test('converts a binary strict equals', () => {
         expect(compiledTest.out.default.binaryStrictEquals).toMatchObject((() => {
             return ExpressionBuilder.binary(
                 ExpressionBuilder.constant(scopeVariable),
-                BinaryExpression.BinaryOperator.strictEquals,
+                BinaryOperator.strictEquals,
                 ExpressionBuilder.constant(5));
         })());
     });
@@ -71,7 +79,7 @@ describe('expression kinds', () => {
         expect(compiledTest.out.default.binaryNotEquals).toMatchObject((() => {
             return ExpressionBuilder.binary(
                 ExpressionBuilder.constant(scopeVariable),
-                BinaryExpression.BinaryOperator.notEquals,
+                BinaryOperator.notEquals,
                 ExpressionBuilder.constant(5));
         })());
     });
@@ -80,12 +88,12 @@ describe('expression kinds', () => {
             return ExpressionBuilder.binary(
                 ExpressionBuilder.binary(
                     ExpressionBuilder.constant(scopeVariable),
-                    BinaryExpression.BinaryOperator.notEquals,
+                    BinaryOperator.notEquals,
                     ExpressionBuilder.constant(1)),
-                BinaryExpression.BinaryOperator.and,
+                BinaryOperator.and,
                 ExpressionBuilder.binary(
                     ExpressionBuilder.constant(scopeVariable),
-                    BinaryExpression.BinaryOperator.equals,
+                    BinaryOperator.equals,
                     ExpressionBuilder.constant(5)));
         })());
     });
@@ -94,12 +102,12 @@ describe('expression kinds', () => {
             return ExpressionBuilder.binary(
                 ExpressionBuilder.binary(
                     ExpressionBuilder.constant(scopeVariable),
-                    BinaryExpression.BinaryOperator.equals,
+                    BinaryOperator.equals,
                     ExpressionBuilder.constant(1)),
-                BinaryExpression.BinaryOperator.or,
+                BinaryOperator.or,
                 ExpressionBuilder.binary(
                     ExpressionBuilder.constant(scopeVariable),
-                    BinaryExpression.BinaryOperator.equals,
+                    BinaryOperator.equals,
                     ExpressionBuilder.constant(5)));
         })());
     });
@@ -107,7 +115,7 @@ describe('expression kinds', () => {
         expect(compiledTest.out.default.binaryPlus).toMatchObject((() => {
             return ExpressionBuilder.binary(
                 ExpressionBuilder.constant(1),
-                BinaryExpression.BinaryOperator.plus,
+                BinaryOperator.plus,
                 ExpressionBuilder.constant(1));
         })());
     });
@@ -116,6 +124,7 @@ describe('expression kinds', () => {
             return ExpressionBuilder.call(
                 ExpressionBuilder.constant(scopeFunction0));
         })());
+        expect(compiledTest.out.default.call0.kind).toBe(ExpressionKind.call);
     });
     test('converts a call (1 arg)', () => {
         expect(compiledTest.out.default.call1).toMatchObject((() => {
@@ -136,6 +145,7 @@ describe('expression kinds', () => {
         expect(compiledTest.out.default.action).toMatchObject((() => {
             return ExpressionBuilder.lambda([], 'todo');
         })());
+        expect(compiledTest.out.default.action.kind).toBe(ExpressionKind.lambda);
     });
     test('converts a lambda (0 args)', () => {
         expect(compiledTest.out.default.lambda0).toMatchObject((() => {
@@ -147,6 +157,7 @@ describe('expression kinds', () => {
             const n = ExpressionBuilder.parameter('n');
             return ExpressionBuilder.lambda([n], n);
         })());
+        expect(compiledTest.out.default.lambda1.parameters[0].kind).toBe(ExpressionKind.parameter);
     });
     test('converts a lambda (2 arg)', () => {
         expect(compiledTest.out.default.lambda2).toMatchObject((() => {
@@ -154,7 +165,7 @@ describe('expression kinds', () => {
             const y = ExpressionBuilder.parameter('y');
             return ExpressionBuilder.lambda(
                 [x, y], 
-                ExpressionBuilder.binary(x, BinaryExpression.BinaryOperator.plus, y));
+                ExpressionBuilder.binary(x, BinaryOperator.plus, y));
         })());
     });
     test('converts an object literal', () => {
@@ -164,6 +175,8 @@ describe('expression kinds', () => {
                 ExpressionBuilder.propertyAssignment('numberProp', ExpressionBuilder.constant(1)),
             ]);
         })());
+        expect(compiledTest.out.default.objectLiteral.kind).toBe(ExpressionKind.objectLiteral);
+        expect(compiledTest.out.default.objectLiteral.properties[0].kind).toBe(ExpressionKind.propertyAssignment);
     });
     test('converts an object literal with shorthand assignments', () => {
         expect(compiledTest.out.default.objectLiteralShorthand).toMatchObject((() => {
@@ -179,5 +192,6 @@ describe('expression kinds', () => {
                 ExpressionBuilder.constant(objectWithProps),
                 'prop1');
         })());
+        expect(compiledTest.out.default.propertyAccess.kind).toBe(ExpressionKind.propertyAccess);
     });
 });
