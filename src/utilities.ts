5dc175b57a4d7d59bfc1e25a5618ea032878d004
Pedro Pedrosa <blastamos@outlook.com>
2018-06-15 21:38:30 +0100

    added type guard functions for expressions

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,12 +1,40 @@
-import { expressionSymbol, Expression } from './expressions/Expression';
 import { ExpressionParameterNotConvertedException } from './exceptions/ExpressionParameterNotConvertedException';
+import * as expr from './expressions';
 
-export function isExpression<T>(expr: object): expr is Expression<T> {
-    return expr.hasOwnProperty(expressionSymbol);
+export function isExpression<T>(o: object): o is expr.Expression<T> {
+    return o.hasOwnProperty(expr.expressionSymbol);
 }
-export function checkExpressionParameter<T>(expr: object, parameterName: string): Expression<T> {
-    if (!isExpression<T>(expr)) {
+export function isArrayLiteralExpression(o: object): o is expr.ArrayLiteralExpression {
+    return isExpression<{}>(o) && o.kind == expr.ExpressionKind.arrayLiteral;
+}
+export function isBinaryExpression(o: object): o is expr.BinaryExpression {
+    return isExpression<{}>(o) && o.kind == expr.ExpressionKind.binary;
+}
+export function isCallExpression(o: object): o is expr.CallExpression {
+    return isExpression<{}>(o) && o.kind == expr.ExpressionKind.call;
+}
+export function isConstantExpression(o: object): o is expr.ConstantExpression {
+    return isExpression<{}>(o) && o.kind == expr.ExpressionKind.constant;
+}
+export function isLambdaExpression(o: object): o is expr.LambdaExpression {
+    return isExpression<{}>(o) && o.kind == expr.ExpressionKind.lambda;
+}
+export function isObjectLiteralExpression(o: object): o is expr.ObjectLiteralExpression {
+    return isExpression<{}>(o) && o.kind == expr.ExpressionKind.objectLiteral;
+}
+export function isParameterExpression(o: object): o is expr.ParameterExpression {
+    return isExpression<{}>(o) && o.kind == expr.ExpressionKind.parameter;
+}
+export function isPropertyAccessExpression(o: object): o is expr.PropertyAccessExpression {
+    return isExpression<{}>(o) && o.kind == expr.ExpressionKind.propertyAccess;
+}
+export function isPropertyAssignmentExpression(o: object): o is expr.PropertyAssignmentExpression {
+    return isExpression<{}>(o) && o.kind == expr.ExpressionKind.propertyAssignment;
+}
+
+export function checkExpressionParameter<T>(o: object, parameterName: string): expr.Expression<T> {
+    if (!isExpression<T>(o)) {
         throw new ExpressionParameterNotConvertedException(parameterName);
     }
-    return expr;
+    return o;
 }
