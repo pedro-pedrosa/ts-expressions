fa8efa945cc7801328394697d1795e049708c870
Pedro Pedrosa <blastamos@outlook.com>
2018-06-16 11:19:54 +0100

    improved expression type guards after refactor

--- src/utilities.ts
+++ src/utilities.ts
@@ -4,32 +4,32 @@ import * as expr from './expressions';
 export function isExpression<T>(o: object): o is expr.Expression<T> {
     return o.hasOwnProperty(expr.expressionSymbol);
 }
-export function isArrayLiteralExpression(o: object): o is expr.ArrayLiteralExpression {
-    return isExpression<{}>(o) && o.root.kind == expr.ExpressionKind.arrayLiteral;
+export function isArrayLiteralExpression<T>(o: object): o is expr.Expression<T> & { root: expr.ArrayLiteralExpression } {
+    return isExpression<T>(o) && o.root.kind == expr.ExpressionKind.arrayLiteral;
 }
-export function isBinaryExpression(o: object): o is expr.BinaryExpression {
-    return isExpression<{}>(o) && o.root.kind == expr.ExpressionKind.binary;
+export function isBinaryExpression<T>(o: object): o is expr.Expression<T> & { root: expr.BinaryExpression } {
+    return isExpression<T>(o) && o.root.kind == expr.ExpressionKind.binary;
 }
-export function isCallExpression(o: object): o is expr.CallExpression {
-    return isExpression<{}>(o) && o.root.kind == expr.ExpressionKind.call;
+export function isCallExpression<T>(o: object): o is expr.Expression<T> & { root: expr.CallExpression } {
+    return isExpression<T>(o) && o.root.kind == expr.ExpressionKind.call;
 }
-export function isConstantExpression(o: object): o is expr.ConstantExpression {
-    return isExpression<{}>(o) && o.root.kind == expr.ExpressionKind.constant;
+export function isConstantExpression<T>(o: object): o is expr.Expression<T> & { root: expr.ConstantExpression } {
+    return isExpression<T>(o) && o.root.kind == expr.ExpressionKind.constant;
 }
-export function isLambdaExpression(o: object): o is expr.LambdaExpression {
-    return isExpression<{}>(o) && o.root.kind == expr.ExpressionKind.lambda;
+export function isLambdaExpression<T>(o: object): o is expr.Expression<T> & { root: expr.LambdaExpression } {
+    return isExpression<T>(o) && o.root.kind == expr.ExpressionKind.lambda;
 }
-export function isObjectLiteralExpression(o: object): o is expr.ObjectLiteralExpression {
-    return isExpression<{}>(o) && o.root.kind == expr.ExpressionKind.objectLiteral;
+export function isObjectLiteralExpression<T>(o: object): o is expr.Expression<T> & { root: expr.ObjectLiteralExpression } {
+    return isExpression<T>(o) && o.root.kind == expr.ExpressionKind.objectLiteral;
 }
-export function isParameterExpression(o: object): o is expr.ParameterExpression {
-    return isExpression<{}>(o) && o.root.kind == expr.ExpressionKind.parameter;
+export function isParameterExpression<T>(o: object): o is expr.Expression<T> & { root: expr.ParameterExpression } {
+    return isExpression<T>(o) && o.root.kind == expr.ExpressionKind.parameter;
 }
-export function isPropertyAccessExpression(o: object): o is expr.PropertyAccessExpression {
-    return isExpression<{}>(o) && o.root.kind == expr.ExpressionKind.propertyAccess;
+export function isPropertyAccessExpression<T>(o: object): o is expr.Expression<T> & { root: expr.PropertyAccessExpression } {
+    return isExpression<T>(o) && o.root.kind == expr.ExpressionKind.propertyAccess;
 }
-export function isPropertyAssignmentExpression(o: object): o is expr.PropertyAssignmentExpression {
-    return isExpression<{}>(o) && o.root.kind == expr.ExpressionKind.propertyAssignment;
+export function isPropertyAssignmentExpression<T>(o: object): o is expr.Expression<T> & { root: expr.PropertyAssignmentExpression } {
+    return isExpression<T>(o) && o.root.kind == expr.ExpressionKind.propertyAssignment;
 }
 
 export function checkExpressionParameter<T>(o: object, parameterName: string): expr.Expression<T> {
